9134efaaac85276dc31789012e301359664c1339
aryangupta1707 <aryangupta.ag1707@gmail.com>
2024-08-24 15:35:52 +0530

    adding reverse.java to master

---
+++ Reverse.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public  class Reverse
+{
+    public static void main(String[] args) {
+        List<Integer> li=Arrays.asList(10,20,30,40,50);
+        Collections.reverse(li);
+        System.out.println(li);
+
+        
+
+
+    }
+}
